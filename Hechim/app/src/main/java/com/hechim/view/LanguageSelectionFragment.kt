56e1eeeba77c93b25f8807981435f57ff2c32e1a
Semir Suljevic <suljevic.semir@gmail.com>
2022-12-28 20:24:55 +0100

    language selection fragment added

---
+++ Hechim/app/src/main/java/com/hechim/view/LanguageSelectionFragment.kt
@@ -0,0 +1,23 @@
+package com.hechim.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hechim.R
+
+
+class LanguageSelectionFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_language_selection, container, false)
+    }
+
+
+}
