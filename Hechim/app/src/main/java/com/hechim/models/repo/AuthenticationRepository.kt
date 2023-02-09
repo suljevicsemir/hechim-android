81bc462a993794b8de5ad3b83a551a620c1ab91e
Semir Suljevic <mase59364@gmail.com>
2023-02-09 21:52:32 +0100

    remove unnecessary imports

--- Hechim/app/src/main/java/com/hechim/models/repo/AuthenticationRepository.kt
+++ Hechim/app/src/main/java/com/hechim/models/repo/AuthenticationRepository.kt
@@ -1,44 +1,29 @@
 package com.hechim.models.repo
 
-import android.content.Context
-import com.hechim.di.SecureSharedPref
 import com.hechim.di.network_module.BaseRepository
 import com.hechim.models.data.Resource
 import com.hechim.models.data.auth.*
 import com.hechim.models.interfaces.api.AuthenticationAPI
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
 @ActivityRetainedScoped
 class AuthenticationRepository @Inject constructor(
     private val authenticationAPI: AuthenticationAPI,
-    private val secureSharedPref: SecureSharedPref,
-    @ApplicationContext applicationContext: Context
 ): BaseRepository(){
     suspend fun register(userRegister: UserRegister): Resource<UserConfirmedRegister> {
         return backendMiddleware { authenticationAPI.register(userRegister) }
     }
 
     suspend fun login(userLogin: UserLogin): Resource<TokenPair>{
-        return backendMiddleware {
-            authenticationAPI.login(userLogin)
-        }
+        return backendMiddleware { authenticationAPI.login(userLogin) }
     }
 
     suspend fun confirmEmail(confirmEmail: ConfirmEmail): Resource<TokenPair>{
-        return backendMiddleware {
-            authenticationAPI.confirmEmail(confirmEmail)
-        }
+        return backendMiddleware { authenticationAPI.confirmEmail(confirmEmail) }
     }
 
     suspend fun refreshToken(tokenPair: TokenPair): Resource<TokenPair>{
-        return backendMiddleware {
-            authenticationAPI.refreshToken(tokenPair)
-        }
+        return backendMiddleware { authenticationAPI.refreshToken(tokenPair) }
     }
-
-
-
-
 }
